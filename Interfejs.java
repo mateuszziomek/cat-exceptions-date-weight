d0ff1ed8694d8f180b08cb1627a98b33d93f84a5
mateuszziomek <matysiatko@gmail.com>
2016-06-03 18:45:00 +0200

    Update Interfejs.java
    Refactored code.
    
    Additional variables deleted.

--- Interfejs.java
+++ Interfejs.java
@@ -2,60 +2,46 @@ package lekcja2;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Interfejs {
 
-	// static methods
-	public static String getUserInput(Scanner scanner) {
-		return scanner.nextLine();
+	// static Scanner - dzieki temu nie potrzeba arg formalnego
+	static Scanner scan = new Scanner(System.in);
+	public static String getUserInput() {
+		return scan.nextLine();
 	}
-
-	// zmienne pomocnicze
-	private static Float wagaKotaf = null;
-	private static String wagaKotas;
-
-	private static Date date = null;
-	private static String dateStr;
 
 	public static void main(String[] args) {
 
 		Kot kot = new Kot();
-		Scanner scan = new Scanner(System.in);
-
+		
 		System.out.println("Podaj imie kota: ");
-		kot.setName(getUserInput(scan));
-		System.out.println(kot.getName());
+		kot.setName(getUserInput());
 
 		System.out.println("Podaj imie opiekuna: ");
-		kot.setOpiekun(getUserInput(scan));
-		System.out.println(kot.getOpiekun());
+		kot.setOpiekun(getUserInput());
 
+		// zmienne pomocnicze niepotrzebne - zapis w jednej linii, choc zlozony
 		do {
 			System.out.println("Podaj wage kota: ");
-			wagaKotas = getUserInput(scan);
 			try {
-				wagaKotaf = Float.parseFloat(wagaKotas);
-				kot.setWaga(wagaKotaf);
+				kot.setWaga(Float.valueOf(getUserInput()));
 			} catch (NumberFormatException nfe) {
-				System.out.println("Wrong string!");
+				System.out.println("Niepoprawne dane. Format wagi: 5.5");
 			}
-		} while (wagaKotaf == null);
+		} while (kot.getWaga() == null);
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
 		do {
-			System.out.println("Podaj date RRRR-MM-DD: ");
-			dateStr = getUserInput(scan);
+			System.out.println("Podaj date w formacie: RRRR-MM-DD: ");
 			try {
-				date = sdf.parse(dateStr);
-				String dateFormat = sdf.format(date);
-				kot.setDate(dateFormat);
+				kot.setDate(sdf.format(sdf.parse(getUserInput())));
 			} catch (ParseException e) {
-				System.out.println("Wrong string!");
+				System.out.println("Niepoprawne dane. Format daty: RRRR-MM-DD");
 			}
-		} while (date == null);
+		} while (kot.getDate() == null);
 
 		System.out.println(kot.toString());
 
